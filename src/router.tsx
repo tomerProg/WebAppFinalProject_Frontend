ac525003b14abd35f211d8303c3b25eee3c64648
omer hasid <omerhasid11@gmail.com>
2025-01-18 22:13:21 +0200

    add router

---
+++ src/router.tsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router-dom";
+import { SignIn } from "./pages/SignIn/SignIn";
+import Register from "./pages/Register/Register";
+
+export const router = createBrowserRouter([
+    { path: '/', element: <SignIn/>},
+    { path: '/register', element: <Register/>}
+])
