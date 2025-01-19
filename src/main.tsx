f6a55deafbfe39ffca0d63f8a32b029d7e04f6d3
Tomer Shomron <tomercpc01@gmail.com>
2025-01-19 19:54:21 +0200

    revert main.tsx to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import PostPage from './pages/PostPage/PostPage';
-// import { UserContext } from './Contexts/UserContext';
-// import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        {/* <UserContext.Provider > */}
-            <PostPage />
-        {/* </UserContext.Provider> */}
-    </StrictMode>
-);
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
