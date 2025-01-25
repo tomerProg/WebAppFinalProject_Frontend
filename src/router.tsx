fe826d8419d53c9fd50bb6302163fd53083ac501
Tomer Shomron <tomercpc01@gmail.com>
2025-01-25 20:51:12 +0200

    add post page to router

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
-import SignIn from "./pages/SignIn/SignIn";
-import Register from "./pages/Register/Register";
+import { createBrowserRouter } from 'react-router-dom';
+import SignIn from './pages/SignIn/SignIn';
+import Register from './pages/Register/Register';
+import PostPage from './pages/PostPage/PostPage';
 
 export const router = createBrowserRouter([
-    { path: '/', element: <SignIn/>},
-    { path: '/register', element: <Register/>}
-])
+    { path: '/', element: <SignIn /> },
+    { path: '/register', element: <Register /> },
+    { path: '/post', element: <PostPage /> }
+]);
