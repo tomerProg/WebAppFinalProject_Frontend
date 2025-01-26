907a296cc0c442341a66236c95c181ae59c01b7d
Tomer Shomron <tomercpc01@gmail.com>
2025-01-26 19:39:02 +0200

    validating post type

--- src/pages/PostPage/types.ts
+++ src/pages/PostPage/types.ts
@@ -1,10 +1,13 @@
-export type Post = {
-    _id: string;
-    title: string;
-    owner: string;
-    description: string;
-    suggastion?: string;
-    likes: string[];
-    dislikes: string[];
-    postImage?: string;
-};
+import { z } from 'zod';
+
+export const postZodSchema = z.object({
+    _id: z.string(),
+    title: z.string(),
+    owner: z.string(),
+    description: z.string(),
+    suggastion: z.string().optional(),
+    likes: z.array(z.string()).default([]),
+    dislikes: z.array(z.string()).default([]),
+    postImage: z.string().optional()
+});
+export type Post = z.infer<typeof postZodSchema>;
