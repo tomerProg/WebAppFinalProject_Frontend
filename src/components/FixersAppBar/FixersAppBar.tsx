6a66e5fb4b5684be4310bfb55b0d254773193505
omer hasid <omerhasid11@gmail.com>
2025-02-10 22:19:50 +0200

    build app bar

---
+++ src/components/FixersAppBar/FixersAppBar.tsx
@@ -0,0 +1,53 @@
+import { FunctionComponent, useContext, useEffect, useState } from 'react';
+import { AppBar, Toolbar, Typography, IconButton, Avatar, Box, Icon} from '@mui/material';
+import { User } from '../../api/users/types';
+import { getUserById } from '../../api/users/users.api';
+import { ignoreCanceledRequest } from '../../api/utils';
+import { withStyles, WithStyles } from '@mui/styles';
+import { styles } from './styles';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { UserIdContext } from '../../Contexts/UserIdContext/UserContext';
+
+const FixersAppBar: FunctionComponent<WithStyles<typeof styles>> = (props) => {
+  const userId = useContext(UserIdContext);  
+  const { classes } = props;
+  const [loginUser, setLoginUser] = useState<User | null>(null);
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+          const { request, abort } = getUserById(userId);
+          request
+              .then(({ data }) => setLoginUser(data))
+              .catch(ignoreCanceledRequest);
+          return () => abort();
+  }, [userId]);
+
+  const handleOpenUserProfile = () => {
+    // navigate('/profile');
+  }
+
+  return (
+    <>
+    <AppBar position="static" color="default" elevation={1}>
+      <Toolbar>
+        <Box className={classes.leftBox}>          
+          <Icon sx={{height: "4rem", width: "3rem"}} >
+            <img src="/favicon.svg" alt="icon" width={50} height={50}/>
+          </Icon>
+          <Typography variant="h4">Fixers</Typography>
+        </Box>
+        <Box className={classes.middleBox}/>                    
+        <Box >          
+          <IconButton edge="end" className={classes.avatarButton} onClick={handleOpenUserProfile}>
+            <Avatar alt="User Avatar" sx={{width: '100%', height: '100%'}} 
+              src={loginUser ? loginUser.profileImageUrl : undefined} />
+          </IconButton>
+        </Box>        
+      </Toolbar>
+    </AppBar>
+    <Outlet />
+    </>
+  );
+};
+
+export default withStyles(styles)(FixersAppBar);
--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,22 @@
 import { Dispatch, SetStateAction } from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, Route, Routes } from 'react-router-dom';
 import PostPage from './pages/PostPage/PostPage';
 import PostsList from './pages/PostsList/PostsList';
 import Register from './pages/Register/Register';
 import SignIn from './pages/SignIn/SignIn';
 import UserProfilePage from './pages/UserProfilePage/UserProfilePage';
+import FixersAppBar from './components/FixersAppBar/FixersAppBar';
 
 export const createRouter = (setUserId: Dispatch<SetStateAction<string>>) =>
-    createBrowserRouter([
-        { path: '/', element: <SignIn setUserId={setUserId} /> },
-        { path: '/register', element: <Register setUserId={setUserId} /> },
-        { path: '/profile', element: <UserProfilePage /> },
-        { path: '/posts', element: <PostsList /> },
-        { path: '/post', element: <PostPage /> }
-    ]);
+    createBrowserRouter(
+        [{
+            path: '/', element: <FixersAppBar />,
+            children: [
+                { path: '/', element: <SignIn setUserId={setUserId} /> },
+                { path: '/register', element: <Register setUserId={setUserId} /> },
+                { path: '/profile', element: <UserProfilePage /> },
+                { path: '/posts', element: <PostsList /> },
+                { path: '/post', element: <PostPage /> }
+            ]
+    }]);
+    
