6569daf84bc8b11617aff30708e1cab997ab14ca
Tomer Shomron <tomercpc01@gmail.com>
2025-02-24 23:28:47 +0200

    end line

--- src/pages/CreatePostPage/components/utils.ts
+++ src/pages/CreatePostPage/components/utils.ts
@@ -1,15 +1,15 @@
-import { PostInput, PostInputError } from "./types";
+import { PostInput, PostInputError } from './types';
 
 export const getPostInputError = (postInput: PostInput): PostInputError => {
     const newErrors: PostInputError = {};
-    
+
     if (!postInput.title.trim()) {
-      newErrors.title = 'Title is required';
+        newErrors.title = 'Title is required';
     }
 
     if (!postInput.description.trim()) {
-      newErrors.description = 'Description is required';
-    } 
-    
+        newErrors.description = 'Description is required';
+    }
+
     return newErrors;
-  };
+};
