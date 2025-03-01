31b8aad2efafff5b68443782c5b9d0e60918b089
Tomer Shomron <tomercpc01@gmail.com>
2025-03-01 23:00:38 +0200

    vite env

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const backendURL = import.meta.env.VITE_BACKEND_URL;
+
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: backendURL,
     withCredentials: true
 });
 
