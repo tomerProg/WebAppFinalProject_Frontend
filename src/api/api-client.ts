ab39948db575b759c90b69b38eb7cc7bda23a50c
Tomer Shomron <tomercpc01@gmail.com>
2025-03-04 00:19:47 +0200

    fix api client

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const backendURL: string = import.meta.env.VITE_BACKEND_URL;
 
 const apiClient = axios.create({
-    baseURL: backendURL.concat('/api'),
+    baseURL: `${backendURL}/api`,
     withCredentials: true
 });
 
