4698651a8146911a291257b144608b4fe8563235
Tomer Shomron <tomercpc01@gmail.com>
2025-03-16 00:27:45 +0200

    names at main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+// tomer-shomron-324205145-omer-hasid-322994120
+
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
